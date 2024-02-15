5480ae19a0ccd5bd2f3d9c4f05b80228da90c36e
mdaftab12 <mahfigma@gmail.com>
2024-02-15 08:48:04 +0530

    Create fackdb

---
+++ fackdb.js
@@ -0,0 +1,31 @@
+export const users = [
+  {
+    id: "23131",
+    firstName: "John",
+    lastName: "Smith",
+    email: "smithjohngmail.com",
+    password: "12345",
+  },
+  {
+    id: "4232",
+    firstName: "xyz",
+    lastName: "abc",
+    email: "abcgmail.com",
+    password: "12346",
+  },
+];
+
+export const quotes = [
+  {
+    name: "I turn coffee into code",
+    by: "23131",
+  },
+  {
+    name: "I am another quote",
+    by: "23131",
+  },
+  {
+    name: "If it works don't touch it",
+    by: "4232",
+  },
+];
--- server.js
+++ server.js
@@ -1,25 +1,43 @@
 import { ApolloServer, gql } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { users, quotes } from "./fackdb.js";
 
 const typeDefs = gql`
   type Query {
-    greet: String
+    users: [User]
+    user(id: ID!): User
+    quotes: [Quotes]
+  }
+  type User {
+    id: ID!
+    firstName: String
+    lastName: String
+    email: String
+    password: String
+    quotes: [Quotes]
+  }
+  type Quotes {
+    by: ID
+    name: String
   }
 `;
 const resolvers = {
   Query: {
-    greet: () => "Hello world",
+    users: () => users,
+    user: (_, { id }) => users.find((user) => user.id === id),
+    quotes: () => quotes,
+  },
+  User: {
+    quotes: (user) => quotes.filter((quote) => quote.by == user.id),
   },
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  plugins:[
-    ApolloServerPluginLandingPageGraphQLPlayground()
-  ]
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at: ${url}`);
+  console.log(`🚀 Server ready at: ${url}`);
 });
